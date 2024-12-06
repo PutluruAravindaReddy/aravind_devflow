e7bfd36977b03f6eadd7b4a60761c9be669c19fd
PutluruAravindaReddy <aravindareddy60@gmail.com>
2024-12-06 16:19:20 +0530

    Project Setup (Tailwind, Meta Data, Images)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 const Home = () => {
   return (
-    <h1 className="text-5xl  font-black text-violet-600">
-      Welcome to World of Next.js
-    </h1>
+    <>
+      <h1 className="text-5xl  font-black text-violet-600">
+        Welcome to World of Next.js
+      </h1>
+      <h1 className="text-5xl  font-black text-violet-600">
+        Welcome to World of Next.js
+      </h1>
+    </>
   );
 };
 
