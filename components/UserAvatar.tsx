b286abffc401edcf17267eccde27b6a92659e9ec
PutluruAravindaReddy <aravindareddy60@gmail.com>
2025-02-20 09:42:08 +0530

    Implemented Fetching Question details

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -3,17 +3,25 @@ import Link from "next/link";
 import React from "react";
 
 import ROUTES from "@/constants/routes";
+import { cn } from "@/lib/utils";
 
 import { Avatar, AvatarFallback } from "./ui/avatar";
 
 interface Props {
   id: string;
   name: string;
-  imageUrl?: string;
+  imageUrl?: string | null;
   className?: string;
+  fallbackClassName?: string;
 }
 
-const UserAvatar = ({ id, name, imageUrl, className = "h-9 w-9" }: Props) => {
+const UserAvatar = ({
+  id,
+  name,
+  imageUrl,
+  className = "h-9 w-9",
+  fallbackClassName,
+}: Props) => {
   const initials = name
     .split(" ")
     .map((word: string) => word[0])
@@ -22,26 +30,29 @@ const UserAvatar = ({ id, name, imageUrl, className = "h-9 w-9" }: Props) => {
     .slice(0, 2);
 
   return (
-    <>
-      <Link href={ROUTES.PROFILE(id)}>
-        <Avatar className={className}>
-          {imageUrl ? (
-            <Image
-              src={imageUrl}
-              alt={name}
-              className="object-cover"
-              width={36}
-              height={36}
-              quality={100}
-            />
-          ) : (
-            <AvatarFallback className="primary-gradient font-space-grotesk font-bold tracking-wider text-white">
-              {initials}
-            </AvatarFallback>
-          )}
-        </Avatar>
-      </Link>
-    </>
+    <Link href={ROUTES.PROFILE(id)}>
+      <Avatar className={className}>
+        {imageUrl ? (
+          <Image
+            src={imageUrl}
+            alt={name}
+            className="object-cover"
+            width={36}
+            height={36}
+            quality={100}
+          />
+        ) : (
+          <AvatarFallback
+            className={cn(
+              "primary-gradient font-space-grotesk font-bold tracking-wider text-white",
+              fallbackClassName
+            )}
+          >
+            {initials}
+          </AvatarFallback>
+        )}
+      </Avatar>
+    </Link>
   );
 };
 
